2246776be5277eacda37e548f6b872905ee379d7
Zahid Hasan <zahidhasan140338@gmail.com>
2023-09-11 11:10:05 +0600

    loaded data for 250 countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+
 import './App.css'
+import Countries from './components/countries/countries'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     
       <h1>React World Tour</h1>
-      
+      <Countries></Countries>
+    
     </>
   )
 }
